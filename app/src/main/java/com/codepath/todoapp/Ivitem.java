03ff3486c9cb124b5bbcf77c2256a6ed48430d98
Fortune Max Abraham <abbyfortune9@gmail.com>
2018-11-03 17:28:07 -0400

    initial commit

---
+++ app/src/main/java/com/codepath/todoapp/Ivitem.java
@@ -0,0 +1,4 @@
+package com.codepath.todoapp;
+
+class Ivitem {
+}
--- app/src/main/java/com/codepath/todoapp/MainActivity.java
+++ app/src/main/java/com/codepath/todoapp/MainActivity.java
@@ -2,24 +2,43 @@ package com.codepath.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> todoItems;
     ArrayAdapter<String> aToDoAdapter;
-    ListVew lvItem;
-    Edit
+    ListView lvItem;
+    EditText etEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         populateArrayItems();
-        ltVitem = (Listvew) findVewById(R.Id.lvItems);
-        lvItems.setAdapter(aToDoAdapter);
+        lvItem = (ListView) findViewById(R.id.Items);
+        lvItem.setAdapter(aToDoAdapter);
+        etEditText = (EditText) findViewById(R.id.etEditText);
+        lvItem.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position,
+                                           long l) {
+                todoItems.remove(position);
+                aToDoAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 
     public void populateArrayItems() {
@@ -27,11 +46,22 @@ public class MainActivity extends AppCompatActivity {
         todoItems.add("Item 1");
         todoItems.add("Item 2");
         todoItems.add("Item 3");
-        aToDoAdapter = new ArrayAdapter<String>(this. android.R. layout.simple_list_item_1, todoItems);
-        
+        aToDoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
 
     }
 
+    private void readItems() {
+        File filesDir=getFilesDir();
+        File file= new File(filesDir,"todo.txt");
+        try {
+            todoItems = new ArrayList<String>(FileUtils.readLines(file));
+        }catch (IOException e) {
+
+        }
+    }
+
     public void onAddItem(View view) {
+        aToDoAdapter.add(etEditText.getText().toString());
+        etEditText.setText("");
     }
 }
